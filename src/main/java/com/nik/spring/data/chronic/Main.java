89f9dd06ab30b5287a4df025f81b67a111d88ea8
Nikhil <nikhilbh@mail.com>
2015-11-17 15:47:59 +0800

    changed path after moving to yarn

--- src/main/java/com/nik/spring/data/chronic/Main.java
+++ src/main/java/com/nik/spring/data/chronic/Main.java
@@ -6,8 +6,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  * hadoop fs -rm -r spark_output
  * hadoop fs -rm -r spark_output/*
+ * hadoop fs -rm -r /user/hduser/spark_output  - new
  * 
  * hadoop fs -cat spark_output/part-r-00000
+ * hadoop fs -cat /user/hduser/spark_output/part-r-00000 - new path
  * */
 public class Main {
     public static void main(String[] arguments) {
